43fae613c941d1cd91a677c0f044880337d00dc4
AnthonyLoche <anthonylocheifc@gmail.com>
2026-03-24 01:14:37 -0300

    FEAT(loading): add custom loading gif

--- src/components/global/loading.jsx
+++ src/components/global/loading.jsx
@@ -1,5 +1,5 @@
-// components/Loading.jsx
 import { useLoading } from "../../hooks";
+import loading_gif from "../../assets/images/loading.gif";
 
 export default function Loading() {
   const { isLoading, currentMessage } = useLoading();
@@ -7,9 +7,13 @@ export default function Loading() {
   if (!isLoading) return null;
 
   return (
-    <div className="fixed inset-0 bg-white/95 backdrop-blur-sm flex items-center justify-center z-50">
-      <div className="flex flex-col items-center gap-4 p-8 bg-white rounded-2xl shadow-xl">
-        <div className="w-12 h-12 rounded-full border-4 border-[#f0fdf4] border-t-[#16a34a] animate-spin" />
+    <div className="fixed inset-0 bg-white backdrop-blur-sm flex items-center justify-center z-50">
+      <div className="flex flex-col items-center gap-4 p-8 bg-white rounded-2xl">
+        <img
+          src={loading_gif}
+          alt="Carregando..."
+          className="w-[50%]  object-contain"
+        />
         <span className="text-sm text-[#16a34a] font-medium tracking-wide">
           {currentMessage}
         </span>
